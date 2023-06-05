a81183fc58ebcd090debf8493c06d3b68870571f
msurve2@uw.edu <msurve2@uw.edu>
2023-06-04 17:41:50 -0700

    updated sql file

--- 1NF.sql
+++ 1NF.sql
@@ -1,28 +1,28 @@
-
-select * from artists ;
-
-select * from tracks ;
-
-create table genres as
--- pulls the right columns for new table 
-with x(genres, firstone, rest) as 
-(select id, substr(genres, 1, instr(genres, ',')-1) 
-    as firstone, substr(genres, instr(genres, ',')+1) 
-    as rest 
-    from artists where genres like "%,%"
--- This is grabbing all of the information from the original table
-union all
-     select id, substr(rest, 1, instr(rest, ',')-1) 
-     as firstone, substr(rest, instr(rest, ',')+1) 
-     as rest from x 
-     where rest like "%,%"
-     )
-select id, 
-    replace(replace(replace(firstone, '[',''), ']', ''),'''', '') 
-    from x union all select 
-    id, replace(replace(replace(rest, '[',''), ']', ''),'''', '') 
-    from x where rest not like "%,%" 
-order by id;
+CREATE TABLE one_nf AS
+SELECT
+    id,
+    REPLACE(REPLACE(REPLACE(REPLACE(genres, ']', ''), '[', ''), '"', ''), '''', '') AS cleaned_genres
+FROM
+    artists;
 
 
+CREATE TABLE final_one AS
+SELECT *, trim(value) AS value
+FROM one_nf,
+	json_each(' [" ' || replace(cleaned_genres, ',', ' ","') || ' "] ')
+WHERE value <> ' ';
+-- remove all extra [,] characters
+UPDATE final_one
+SET value = REPLACE(value, '[', '');
+UPDATE final_one
+SET value = REPLACE(value, ']', '');
+UPDATE final_one
+SET value = REPLACE(value, '''', '');
 
+
+CREATE TABLE final_one_nf AS
+SELECT id, value AS genre
+FROM final_one;
+
+DROP TABLE final_one;
+DROP TABLE one_nf;
